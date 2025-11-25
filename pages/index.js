6916caba6aa1d325327239b3ba903107dedb9d58
Alexandre <83110476+AlexandreAesir@users.noreply.github.com>
2025-11-25 17:29:01 +0000

    pagina de construção

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 function Home() {
-  return <h1>🚧Site em construção mas ja é alguma coisa🚧😎</h1>;
+  return (
+    (<h1>🚧Site em construção mas ja é alguma coisa🚧😎</h1>),
+    (
+      <p>
+        Estamos construindo um local para quem busca discuções e para quem quer
+        compartilhar experiencias e dicas de games. Aqui é um local seguro onde
+        pode se conectar com pessoas com o mesmo gosto que você. 🎮
+      </p>
+    )
+  );
 }
 
-function teste() {
-  console.log("Teste");
-}
-
-function teste2() {
-  console.log("teste de identacao");
-}
 export default Home;
