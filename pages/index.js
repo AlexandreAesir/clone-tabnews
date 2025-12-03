44e44109cc2d1696660a4dee87b55dd3d7f524ef
Alexandre <83110476+AlexandreAesir@users.noreply.github.com>
2025-12-03 20:38:22 +0000

    Instalando testrunner

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,10 @@
 function Home() {
-  return (
-    (<h1>🚧Site em construção mas ja é alguma coisa🚧😎</h1>),
-    (
-      <p>
-        Estamos construindo um local para quem busca discuções e para quem quer
-        compartilhar experiencias e dicas de games. Aqui é um local seguro onde
-        pode se conectar com pessoas com o mesmo gosto que você. 🎮
-      </p>
-    )
-  );
+  return <h1>🚧Site em construção mas ja é alguma coisa🚧😎</h1>;
+  <p>
+    Estamos construindo um local para quem busca discuções e para quem quer
+    compartilhar experiencias e dicas de games. Aqui é um local seguro onde pode
+    se conectar com pessoas com o mesmo gosto que você. 🎮
+  </p>;
 }
 
 export default Home;
